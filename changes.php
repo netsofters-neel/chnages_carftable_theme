c2d34254c92b03bd7347d2b92170463c6eb7b2fc
netsofters-neel <126561179+netsofters-neel@users.noreply.github.com>
2024-07-29 12:23:29 +0530

    Update changes.php

--- changes.php
+++ changes.php
@@ -11,7 +11,18 @@ to
 card-content
 
 
+
+
 ------------------------vendor\brackets\admin-generator\resources\views\index.blade.php
+
+
+fa fa-align-justify
+to
+ti-align-justify
+
+
+
+
   <td>
       <div class="table-icons">
               <a class="btn btn-simple btn-info btn-icon table-action view" :href="item.resource_url + '/edit'" title="@{{ trans('brackets/admin-ui::admin.btn.edit') }}" role="button"><i class="ti-pencil-alt"></i></a>
@@ -21,3 +32,30 @@ card-content
           </form>
       </div>
   </td>
+
+
+----------------------- ALL index.blade.php 
+serch -> col col-lg-7 col-xl-5 form-group
+
+                            <form @submit.prevent="">
+                                <div class="row justify-content-md-between">
+                                        <div class="col col-lg-12 col-xl-5 form-group search-box">
+                                            <div class="input-group search-bar">
+                                                <input class="form-control" placeholder="{{ trans('brackets/admin-ui::admin.placeholder.search') }}" v-model="search" @keyup.enter="filter('search', $event.target.value)" />
+                                                <span class="btn btn-primary input-group-addon search-logo" @click="filter('search', search)"><i class="ti-search"></i></span>
+                                            </div>
+
+                                            <label>
+                                                <select class="form-control" v-model="pagination.state.per_page">
+                                                    <option value="10">10</option>
+                                                    <option value="25">25</option>
+                                                    <option value="100">100</option>
+                                                </select>
+                                            </label>
+                                        </div>
+                                </div>
+
+
+                            </form>
+
+
